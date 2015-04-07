d7569d1261358f294a0eb34a651f364ba4fdd30c
Sean Flanigan <sflaniga@redhat.com>
2015-04-07 14:43:12 +1000

    Add WildFly module for Undertow

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ zanata-wildfly contains files which help Zanata (3.5 or later) to run on WildFly
 Author: Sean Flanigan <sflaniga@redhat.com>
 
 
-The modules contain repackaged copies of Mojarra 2.1 and Hibernate 4.2.  The zip files are meant to be extracted into a WildFly installation, which will add some Mojarra 2.1 modules and replace the 'main' Hibernate module with Hibernate 4.2.
+The modules contain repackaged copies of Mojarra 2.1, Hibernate 4.2 and Undertow 1.2.x.  The zip files are meant to be extracted into a WildFly 8.1/8.2 installation, which will add some Mojarra 2.1 modules, replace the 'main' Hibernate module with Hibernate 4.2, and upgrade Undertow to fix https://bugzilla.redhat.com/show_bug.cgi?id=1197955.
 
 The 'main' Hibernate module is used by default, but the Mojarra modules need to be activated by a line like this in `standalone.xml`:
 
@@ -14,12 +14,13 @@ The 'main' Hibernate module is used by default, but the Mojarra modules need to 
 The `standalone` directory (in the source tree) contains configuration for WildFly which might help you to run Zanata.  Note: these files may or may not be kept up to date, and might be removed in future.
 
 
-The build scripts are licensed under LGPL 2.1, but Mojarra and Hibernate retain their original licences.
+The build scripts are licensed under LGPL 2.1, but Mojarra, Hibernate and Undertow retain their original licences.
 
 The source code for zanata-wildfly lives here: https://github.com/zanata/zanata-wildfly
 
  * Module binaries: https://sourceforge.net/projects/zanata/files/wildfly/
  * Hibernate: http://hibernate.org/
  * Mojarra: https://javaserverfaces.java.net/
+ * Undertow: http://undertow.io/
  * WildFly: http://wildfly.org/
  * Zanata: http://zanata.org/
--- build.sh
+++ build.sh
@@ -9,15 +9,21 @@ version_wildfly=8.1.0.Final
 version_hibernate=4.2.15.Final
 version_mojarra=2.1.28
 version_mysql_connector=5.1.32
+version_undertow=1.2.0.Beta10
 
-build_mojarra=true
+build_mojarra=false
 build_hibernate=true
 build_wildfly=true
+build_undertow=true
 
 # rm -fr $target
-rm -fr $target/*.zip $target/*.gz $target/*.cli $target/mojarra21 $target/standalone
+rm -fr $target/*.zip $target/*.gz $target/*.cli \
+  $target/mojarra21 $target/hibernate42 $target/undertow $target/standalone
 
-mkdir -p $target $target/wildfly $target/wildfly-src $target/mojarra21/modules $target/hibernate42/modules/org/hibernate/main $target/standalone/deployments
+mkdir -p $target $target/wildfly $target/wildfly-src \
+  $target/mojarra21/modules $target/hibernate42/modules/org/hibernate/main \
+  $target/undertow/modules/io/undertow/core/main \
+  $target/standalone/deployments
 
 mvn="mvn -q"
 download_artifact=com.googlecode.maven-download-plugin:download-maven-plugin:1.2.0:artifact
@@ -32,6 +38,7 @@ echo 'Getting mysql-connector-java'
 $mvn $download_artifact -DgroupId=mysql -DartifactId=mysql-connector-java -Dversion=${version_mysql_connector} -DoutputDirectory=$target/standalone/deployments/ -DoutputFileName=mysql-connector-java.jar
 
 if $build_mojarra; then
+    module_zip=$target/wildfly-${version_wildfly}-module-mojarra-${version_mojarra}.zip
 
     # Ref: https://community.jboss.org/wiki/StepsToAddAnyNewJSFImplementationOrVersionToWildFly
 
@@ -60,17 +67,21 @@ if $build_mojarra; then
 
     cd $target/wildfly/wildfly-${version_wildfly}/
     echo 'Building Mojarra module zip'
-    zip -qr $target/wildfly-${version_wildfly}-module-mojarra-${version_mojarra}.zip modules/**/mojarra-${version_mojarra}
+    zip -qr $module_zip modules/**/mojarra-${version_mojarra}
 
     cd $projectdir
 
+    echo "The Mojarra module can be found here: $module_zip"
 fi
 
 
 if $build_hibernate; then
+    module_src=$target/wildfly/wildfly-${version_wildfly}/modules/system/layers/base/org/hibernate/4.1
+    module_target=$target/hibernate42/modules/org/hibernate/main
+    module_zip=$target/wildfly-${version_wildfly}-module-hibernate-main-${version_hibernate}.zip
 
-    /bin/cp target/wildfly/wildfly-${version_wildfly}/modules/system/layers/base/org/hibernate/4.1/* $target/hibernate42/modules/org/hibernate/main/
-    sed -i $target/hibernate42/modules/org/hibernate/main/module.xml \
+    /bin/cp $module_src/* $module_target/
+    sed -i $module_target/module.xml \
         -e 's/ slot="4.1">/>/' \
         -e '/<resources>/a\
         <resource-root path="hibernate-core-'${version_hibernate}'.jar"/>\
@@ -78,12 +89,37 @@ if $build_hibernate; then
         <resource-root path="hibernate-infinispan-'${version_hibernate}'.jar"/>'
 
     echo 'Getting Hibernate jars'
-    $mvn $download_artifact -DgroupId=org.hibernate -DartifactId=hibernate-core -Dversion=${version_hibernate} -DoutputDirectory=$target/hibernate42/modules/org/hibernate/main/
-    $mvn $download_artifact -DgroupId=org.hibernate -DartifactId=hibernate-entitymanager -Dversion=${version_hibernate} -DoutputDirectory=$target/hibernate42/modules/org/hibernate/main/
-    $mvn $download_artifact -DgroupId=org.hibernate -DartifactId=hibernate-infinispan -Dversion=${version_hibernate} -DoutputDirectory=$target/hibernate42/modules/org/hibernate/main/
+    $mvn $download_artifact -DgroupId=org.hibernate -DartifactId=hibernate-core -Dversion=${version_hibernate} -DoutputDirectory=$module_target
+    $mvn $download_artifact -DgroupId=org.hibernate -DartifactId=hibernate-entitymanager -Dversion=${version_hibernate} -DoutputDirectory=$module_target
+    $mvn $download_artifact -DgroupId=org.hibernate -DartifactId=hibernate-infinispan -Dversion=${version_hibernate} -DoutputDirectory=$module_target
 
 
     echo 'Building Hibernate module zip'
-    (cd $target/hibernate42 && zip -qr $target/wildfly-${version_wildfly}-module-hibernate-main-${version_hibernate}.zip .)
+    (cd $target/hibernate42 && zip -qr $module_zip .)
 
+    echo "The Hibernate module can be found here: $module_zip"
+fi
+
+if $build_undertow; then
+    module_src=$target/wildfly/wildfly-${version_wildfly}/modules/system/layers/base/io/undertow/core/main
+    module_target=$target/undertow/modules/io/undertow/core/main
+    module_zip=$target/wildfly-${version_wildfly}-module-undertow-${version_undertow}.zip
+
+    if [[ -f $module_src/undertow-core-1.[01].*.jar ]]; then
+        echo "Undertow 1.0 or 1.1 not found in specified version of WildFly. You might need to disable this module by setting build_undertow=false."
+        exit 1
+    fi
+    /bin/cp $module_src/module.xml \
+      $module_target/module.xml
+    sed -i $module_target/module.xml \
+      -e "s/undertow-core-\(1\.[01]\..*\)\.jar/undertow-core-${version_undertow}.jar/"
+
+    echo 'Getting Undertow jar'
+    $mvn $download_artifact -DgroupId=io.undertow -DartifactId=undertow-core -Dversion=${version_undertow} \
+      -DoutputDirectory=$module_target
+
+    echo 'Building Undertow module zip'
+    (cd $target/undertow && zip -qr $module_zip .)
+
+    echo "The Undertow module can be found here: $module_zip"
 fi
